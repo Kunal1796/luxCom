690916fce1e90c2dfb6a54bd53baf4d74ba5a77d
Knal1796 <knal.dd.dongare@gmail.com>
2026-06-28 16:13:51 +0530

    added tailwind

--- LuxCom/src/App.tsx
+++ LuxCom/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900">
       <h1 className="text-5xl font-bold text-white">
-        Tailwind is Working 🚀
+        LuxCom 🚀
       </h1>
     </div>
   );
